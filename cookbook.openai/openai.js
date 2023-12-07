4f3f63539c101d0a1fc208fc1b361548759ebed0
Arafat M <19172+arafatm@users.noreply.github.com>
2023-12-07 14:11:08 -0800

    dotenv

--- cookbook.openai/openai.js
+++ cookbook.openai/openai.js
@@ -1,10 +1,12 @@
 // https://platform.openai.com/docs/libraries/node-js-library
 // $ npm install openai
 import { OpenAI } from "openai";
+import 'dotenv/config';
+
+const openai_key = process.env.OPENAI_API_KEY;
+console.log(openai_key)
  
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
 });
-
-console.log(process.env.OPENAI_API_KEY)
